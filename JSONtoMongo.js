952247c4e22fb8eedc87ed56de35dd37ce6e44fa
Derek <39328219+dpadilla1@users.noreply.github.com>
2019-09-08 23:42:54 -0700

    Bootcamp 2 Completed.

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -12,6 +12,11 @@ var fs = require('fs'),
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
+mongoose.connect(config.db.uri, { useNewUrlParser: true });
+
+mongoose.connection.once('open', function() {
+  console.log(`Connected to MongoDB instance at ${config.db.uri}`);
+});
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
@@ -20,6 +25,22 @@ var fs = require('fs'),
 
   Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
  */
+
+// var x = Listing.Listing
+var listingData;
+
+fs.readFile('listings.json', 'utf8', function(err, data) {
+
+  listingData = JSON.parse(data);
+  listingData.entries.forEach(function(item, index) {
+
+    var x =  new Listing(item);
+    x.save(function(err) {
+      if(err) throw err;
+    });
+    
+  });
+});
 
 
 /*  
